f160be3fb09d0caa09d95df38bb779df91763b82
ameld <73698073+amel1210@users.noreply.github.com>
2021-01-04 13:49:13 +0700

    crud-individu
    Note : kurang muncul notifikasi

---
+++ tampilan.php
@@ -0,0 +1,30 @@
+<head>
+<link rel="stylesheet" href="style1.css">
+<style ="text/css" 	background= url(0.jpg)></style>
+<title>Pendaftaran Siswa Baru</title>
+<body> 
+    <div class="wrapper">
+        <header>
+          <ul>
+		    <h1>TubeTu </h1>
+            <h3>By Ameld </h3>
+          </ul>
+        </header>
+<section class="courses">
+    <article>
+        <font color="darkred" >
+        <h2 align="left" >Pendaftaran Siswa Baru </h2>
+        <h3 align="left">Menu</h3>
+        </font>
+			<div style="border: 1px solid; padding: 1px; height: 65px; width: 200px; text-align: left"> 
+            <li><a href="form_simpan.php">Daftar Baru</a></li>
+            <li><a href="index.php">Pendaftar</a></li>
+			<div>
+    </article>
+</section>
+<footer>
+    &copy; 2020
+</footer>
+</div><!-- .wrapper -->
+</body>
+</html>
